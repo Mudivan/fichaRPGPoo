bd2cc144ef6e750b302ec45fca71555b6e97bb1d
Ivan Carlos <83560584+Mudivan@users.noreply.github.com>
2025-08-30 15:30:05 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,53 @@
+﻿namespace fichaRpg
+{
+    internal class Personagem
+    {
+        //Identifi~cação 
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+
+        //Atributos da Ficha
+        public int Forca { get; set; }
+        public int Habilidade { get; set; }
+        public int Resistencia { get; set; }
+        public int Armadura { get; set; }
+        public int PoderDeFogo { get; set; }
+
+
+        //Recursos
+        public int PontosDeVida {  get; set; }
+        public int PontosDeMagia { get; set; }
+
+        //Outros
+        public List<string> MagiasConhecidas { get; set; }
+        public List<string> Vantagens {  get; set; }
+        public List<string> Desvantagens { get; set; }
+        public List<string> Itens {  get; set; }
+
+        //Metódos
+        public void Atacar()
+        {
+            Console.WriteLine($"{Nome} atacou com seu {Itens[0]}!");
+        }
+
+        public void Defender()
+        {
+            Console.WriteLine($"{Nome} se defendeu com Armadura {Armadura}!");
+        }
+
+        public void LançarMagia(string magia)
+        {
+            if (MagiasConhecidas.Contains(magia) && PontosDeMagia > 0)
+            {
+                PontosDeMagia -= 1; // Gasta 1 PM
+                Console.WriteLine($"{Nome} lançou a magia {magia}!");
+            }
+            else
+            {
+                Console.WriteLine($"{Nome} tentou lançar {magia}, mas falhou!");
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using fichaRpg;
+
+class Program
+{
+    static void Main()
+    {
+        Personagem heroi = new Personagem
+        {
+            Nome = "Kratos",
+            Classe = "Guerreiro",
+            Forca = 3,
+            Habilidade = 2,
+            Resistencia = 4,
+            Armadura = 1,
+            PoderDeFogo = 0,
+            PontosDeVida = 20,
+            PontosDeMagia = 5,
+            MagiasConhecidas = new List<string> { "Cura", "Bola de fogo" },
+            Vantagens = new List<string> { "Ataque Preciso" },
+            Desvantagens = new List<string> { "Medo de Altura" },
+            Itens = new List<string> { "Machado Barbáro", "Escudo" }
+        };
+
+        heroi.Atacar();
+        heroi.Defender();
+        heroi.LançarMagia("cura");
+    }
+
+}
